dacac554996b7033b9715f492e8008ce15a2d5eb
angelojbgama <angelojbgama@gmail.com>
2024-09-08 22:15:58 -0300

    FEAT: Implementação do app Usuarios. CRUD pronto

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,7 +1,60 @@
 from django import forms
+from django.contrib.auth.forms import SetPasswordForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
 
 class PasswordResetCustomForm(forms.Form):
     email = forms.EmailField(
         label="E-mail",
-        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Digite seu e-mail'})
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Digite seu e-mail'
+        })
     )
+
+
+class CustomSetNewPasswordForm(SetPasswordForm):
+    def clean_new_password1(self):
+        password = self.cleaned_data.get('new_password1')
+
+        # Valida a complexidade da senha
+        if len(password) < 8:
+            raise ValidationError("A nova senha deve ter pelo menos 8 caracteres.")
+        if not any(char.isdigit() for char in password):
+            raise ValidationError("A nova senha deve conter pelo menos um número.")
+        if not any(char.isalpha() for char in password):
+            raise ValidationError("A nova senha deve conter pelo menos uma letra.")
+        
+        return password
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label='Senha',
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label='Confirme a Senha',
+        widget=forms.PasswordInput,
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("As senhas não coincidem")
+
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,17 +1,26 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView
 from django.contrib.auth import views as auth_views
+
+from .views import (
+    RegisterView,
+    CustomPasswordResetView,
+    CustomPasswordResetDoneView,
+    CustomPasswordResetConfirmView,
+    CustomPasswordResetCompleteView
+)
 
 
 urlpatterns = [
     path('cadastro/', RegisterView.as_view(), name='cadastro'),
     
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_realized.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_confirm_new_reset.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
+    path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
